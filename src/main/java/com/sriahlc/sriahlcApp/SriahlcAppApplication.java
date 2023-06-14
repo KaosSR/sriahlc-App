7d76a7b7f810df08073bfb3ffcbb4d7ef72b07d8
KaosSR <yordansalvadorromani@gmail.com>
2023-06-14 15:31:30 -0500

    Initial commit

---
+++ src/main/java/com/sriahlc/sriahlcApp/SriahlcAppApplication.java
@@ -0,0 +1,13 @@
+package com.sriahlc.sriahlcApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SriahlcAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SriahlcAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sriahlc/sriahlcApp/SriahlcAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sriahlc.sriahlcApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SriahlcAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
